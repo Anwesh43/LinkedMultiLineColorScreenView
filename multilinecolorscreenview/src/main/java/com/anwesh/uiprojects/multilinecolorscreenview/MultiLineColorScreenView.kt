255b00452c97e52614938f4b25f3aac5b5e34980
Anwesh43 <anweshthecool0@gmail.com>
2019-09-13 10:41:35 +0530

    adding State class

--- multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
+++ multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
@@ -57,7 +57,7 @@ class MultiLineColorScreenView(ctx : Context) : View(ctx) {
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas : Canvas) {
-        
+
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
@@ -67,5 +67,25 @@ class MultiLineColorScreenView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += dir * scGap
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
