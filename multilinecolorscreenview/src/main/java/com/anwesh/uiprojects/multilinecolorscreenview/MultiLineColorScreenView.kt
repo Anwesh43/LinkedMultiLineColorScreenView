b67bed220baca9f83ca73d44a5e45f2bcbb00ce3
Anwesh43 <anweshthecool0@gmail.com>
2019-09-13 10:59:04 +0530

    adding renderer in view

--- multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
+++ multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
@@ -56,15 +56,16 @@ fun Canvas.drawMLCSNode(i : Int, scale : Float, sc : Float, currI : Int, paint :
 class MultiLineColorScreenView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -191,7 +192,7 @@ class MultiLineColorScreenView(ctx : Context) : View(ctx) {
         private val mlcs : MultiLineColorScreen = MultiLineColorScreen(0)
         private val animator : Animator = Animator(view)
 
-        fun draw(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas, paint : Paint) {
             canvas.drawColor(backColor)
             mlcs.draw(canvas, paint)
             animator.animate {
