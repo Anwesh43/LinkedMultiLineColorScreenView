e054973a0f93006cd62ed9617fbaec97e83de902
Anwesh43 <anweshthecool0@gmail.com>
2019-09-13 10:24:38 +0530

    adding libraries and constants

--- multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
+++ multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
@@ -3,3 +3,16 @@ package com.anwesh.uiprojects.multilinecolorscreenview
 /**
  * Created by anweshmishra on 13/09/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#9C27B0", "#2196F3", "#00C853", "#1A237E", "#f44336")
+val lines : Int = 5
+val scGap : Float = 0.01f / lines
+val strokeFactor : Int = 40
