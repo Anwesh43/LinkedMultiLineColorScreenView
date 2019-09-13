4bfc7d8f42ca92e831fbbfd691eb7aef42059278
Anwesh43 <anweshthecool0@gmail.com>
2019-09-13 10:26:30 +0530

    adding Int and Float extension methods

--- multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
+++ multilinecolorscreenview/src/main/java/com/anwesh/uiprojects/multilinecolorscreenview/MultiLineColorScreenView.kt
@@ -16,3 +16,7 @@ val colors : Array<String> = arrayOf("#9C27B0", "#2196F3", "#00C853", "#1A237E",
 val lines : Int = 5
 val scGap : Float = 0.01f / lines
 val strokeFactor : Int = 40
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
